2bc10aaaca101fd7eeb41ccd8052c944ad8a16bd
Artem_Volchugin <volchugin.artem@gmail.com>
2025-01-22 21:05:01 +0300

    new system time control and modification schemes

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ ser = serial.Serial('COM4', 9600)
 xl1 = pd.read_excel('C:/Users/Artem V/PycharmProjects/pythonProject/ПредПроф/RFID/database.xlsx') #Путь к таблице
 data = time.localtime(time.time())
 
-UID_match = 0  # Проверка совпадения UID
-data_match= 0  # Проверка срока работы метки
-
+UID_match = 0  # Проверка совпадения UIDdata_match= 0  # Проверка срока работы метки
 date1 = xl1.iloc[0, 2]
 day1 = str(date1)[8:10]
 month1 = str(date1)[5:7]
@@ -31,31 +29,48 @@ UID3 = xl1.iloc[2, 1]
 while True:
     UID_match = 0
     data_match = 0
-
     UID = ser.readline().decode('utf-8')[:12]
 
     print(UID)
 
     if UID == UID1:
         UID_match = 1
-        if data.tm_year <= int(year1) and data.tm_mon <= int(month1) and data.tm_mday <= int(day1):
+        if data.tm_year < int(year1):
             data_match = 1
+        elif data.tm_year == int(year1):
+            if data.tm_mon < int(month1):
+                data_match = 1
+            elif data.tm_mon == int(month1):
+                if data.tm_mday <= int(day1):
+                    data_match = 1
     elif UID == UID2:
         UID_match = 1
-        if data.tm_year <= int(year2) and data.tm_mon <= int(month2) and data.tm_mday <= int(day2):
+        if data.tm_year < int(year2):
             data_match = 1
+        elif data.tm_year == int(year2):
+            if data.tm_mon < int(month2):
+                data_match = 1
+            elif data.tm_mon == int(month2):
+                if data.tm_mday <= int(day2):
+                    data_match = 1
     elif UID == UID3:
         UID_match = 1
-        if data.tm_year <= int(year3) and data.tm_mon <= int(month3) and data.tm_mday <= int(day3):
+        if data.tm_year < int(year3):
             data_match = 1
+        elif data.tm_year == int(year3):
+            if data.tm_mon < int(month3):
+                data_match = 1
+            elif data.tm_mon == int(month3):
+                if data.tm_mday <= int(day3):
+                    data_match = 1
 
     if UID_match == 1 and data_match == 1:
         print('Доступ разрешён')
         ser.write(b'granted')
+
     elif UID_match == 1 and data_match == 0:
         print('Срок использования карточки закончен')
         ser.write(b'dateout')
     else:
         print('Карточки нет в базе данных')
         ser.write(b'notfind')
-
